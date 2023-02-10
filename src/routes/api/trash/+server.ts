da63231df1ce01c6d1d6da8d98224ed3e963a8e3
Nico-Mayer <nico.m94@icloud.com>
2023-02-10 23:31:51 +0100

    cleaned

--- src/routes/api/trash/+server.ts
+++ src/routes/api/trash/+server.ts
@@ -3,11 +3,9 @@ import type { RequestHandler } from './$types'
 export const GET: RequestHandler = async ({ locals }) => {
 	async function getTrashedNotes(locals: Locals): Promise<Note[]> {
 		if (!locals.session) return []
-		const { id: owner } = locals?.session?.user
 		const { data: notes, error: err } = await locals.sb
 			.from('note')
 			.select('*')
-			.eq('owner', owner)
 			.eq('trash', true)
 			.order('created_at', { ascending: true })
 
@@ -21,11 +19,9 @@ export const GET: RequestHandler = async ({ locals }) => {
 
 	async function getTrashedFolders(locals: Locals): Promise<Folder[]> {
 		if (!locals.session) return []
-		const { id: owner } = locals?.session?.user
 		const { data: folders, error: err } = await locals.sb
 			.from('folder')
 			.select('*')
-			.eq('owner', owner)
 			.eq('trash', true)
 			.order('created_at', { ascending: true })
 
